a7a1d920447f6ac46aa176607f4aaa995f21f039
Jersson Giomar Arrivasplata Rojas <jgrojas@stefanini.com>
2024-01-07 00:05:53 -0500

    chore: Init proyect

---
+++ src/test/java/com/jersson/arrivasplata/swtvap/discoveryserver/swtvapsrvdiscovery/DiscoveryserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jersson.arrivasplata.swtvap.discoveryserver.swtvapsrvdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
